3fcc895a8deb985ee807fd453d0bbf63fa9496cb
Austin Conner <aconner.vu@gmail.com>
2025-11-21 12:25:11 -0500

    method to construct rank 1s with given orbit structure from a parameter space

---
+++ symmetry_search.py
@@ -0,0 +1,56 @@
+from sage.all import *
+from itertools import product
+import numpy as np
+
+def get_map_to_rank1s(rep,orbit_structure):
+    g = rep['g']
+    tripf = rep['tripf']
+    fac_perm_map = rep['fac_perm_map']
+    def action_on_trip(e,M=None):
+        u,v,w = [matrix(UniversalCyclotomicField(),m) for m in tripf(e)]
+        factor_actions = ( w.T.tensor_product(~u), u.T.tensor_product(~v), v.T.tensor_product(~w) )
+        fac_perm = e ** fac_perm_map
+        if M is None:
+            M = gap.PermutationMat(fac_perm,3).sage()
+        blocks = [[ e * act for e in r ] for r, act in zip(M,factor_actions) ]
+        if fac_perm.SignPerm() == -1:
+            def trans(nn):
+                n = sqrt(nn)
+                return matrix([[1 if i1==j and j1==i else 0 for i1,j1 in product(range(n),range(n))] 
+                               for i,j in product(range(n),range(n))])
+            blocks = [[0 if b == 0 else b*trans(b.nrows()) for b in r] for r in blocks ] 
+        return block_matrix(blocks)
+    orbit_embeds = []
+    params = 0
+    for h, fac_mults in list(orbit_structure):
+        eqs = []
+        for e, M in zip(h.GeneratorsOfGroup(), fac_mults.sage()):
+            act = -action_on_trip(e,M)
+            act += identity_matrix(act.nrows())
+            eqs.append(act)
+        eqs = block_matrix([eqs])
+        
+        embed1 = eqs.left_kernel_matrix() # can pick favorite basis here
+        
+        embeds = []
+        for e in g.RightCosets(h):
+            e = e.Representative()
+            embeds.append(embed1 * action_on_trip(e))
+        embeds = np.array(embeds).transpose(1,0,2) # (input, orbitsize, triple)
+        try:
+            embeds = embeds.astype(np.double)
+        except TypeError:
+            embeds = embeds.astype(np.complex128)
+        xbounds = (params, params+embed1.nrows())
+        orbit_embeds.append((embeds,xbounds))
+        params += embed1.nrows()
+
+    u,v,w = [int(m[1].Length()) for m in tripf(g.Identity())]
+    def param_to_trips(x,np=np):
+        res = np.vstack([ np.einsum('ijk,i->jk', embed, x[xstart:xend]) for embed, (xstart, xend) in orbit_embeds ])
+        return (res[:, :w*u], res[:, w*u:w*u+u*v], res[:, w*u+u*v:])
+    
+    # return orbit_embeds
+    return param_to_trips, params
+        
+    
